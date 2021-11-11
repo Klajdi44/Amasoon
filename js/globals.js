b5afabe3641c1d0ffc47b87947327fd139bad861
klajdi44 <klajdi1996@yahoo.gr>
2021-11-11 01:00:18 +0100

    feature/added frontend validation on new password

---
+++ js/globals.js
@@ -0,0 +1,4 @@
+export const _USERNAME_MIN_LEN = 1;
+export const _USERNAME_MAX_LEN = 50;
+export const _PASSWORD_MIN_LEN = 8;
+export const _PASSWORD_MAX_LEN = 20;
--- new-password.php
+++ new-password.php
@@ -13,13 +13,13 @@ require_once(__DIR__ . '/components/top.php');
 			<legend>
 				<label for="user_password">Password</label>
 				<small>At least 8 characters</small>
-				<input id="user_password" class="user_password" name="user_password" type="password" placeholder=" ">
+				<input minlength="8" maxlength="20" id="user_password" class="user_password" name="user_password" type="password" placeholder=" ">
 			</legend>
 
 			<legend>
-				<label for="re-enter_user_password">Confirm password</label>
+				<label for="confirm_user_password">Confirm password</label>
 				<small>Must match password above</small>
-				<input id="confirm_user_password" class="re-enter_user_password" name="re-enter_user_password" type="password" placeholder=" ">
+				<input minlength="8" maxlength="20" id="confirm_user_password" class="confirm_user_password" name="confirm_user_password" type="password" placeholder=" ">
 			</legend>
 
 			<button class="new__password__btn primary__btn" onclick="createNewPassword()">Create</button>
@@ -30,7 +30,10 @@ require_once(__DIR__ . '/components/top.php');
 
 <script>
 	async function createNewPassword() {
+		<?php include_once(__DIR__ . "./private/globals.php") ?>
 		const infoElement = document.querySelector('.new__password__info');
+		const password = document.querySelector('.user_password');
+		const confirmPassword = document.querySelector('.confirm_user_password');
 
 		const urlParams = new URLSearchParams(window.location.search);
 
@@ -38,8 +41,28 @@ require_once(__DIR__ . '/components/top.php');
 		const formData = new FormData(event.target.form)
 		formData.append('key', key);
 
+		//validation
 		if (!key || key?.length != 32) {
 			return infoElement.textContent = "Suspicious"
+		}
+
+		if (!password.value.length || !confirmPassword.value.length) {
+			return infoElement.textContent = "Fields cannot be empty!"
+		}
+
+		if (password.value != confirmPassword.value) {
+			return infoElement.textContent = "Passwords do not match!"
+		}
+
+		if (password.value.length < 8) {
+			return infoElement.textContent = "Password has be at least 8 characters long"
+		}
+		if (password.value.length > 16) {
+			return infoElement.textContent = "Password cannot be more that 16 characters"
+		}
+
+		if (password.value != confirmPassword.value) {
+			return infoElement.textContent = "Passwords do not match!"
 		}
 
 		try {
@@ -53,9 +76,12 @@ require_once(__DIR__ . '/components/top.php');
 
 			infoElement.textContent = response?.info;
 
-
 			if (request.ok) {
 				infoElement.classList.add('success');
+
+				setTimeout(() => {
+					window.location.href = "login";
+				}, 2000);
 			}
 		} catch (error) {
 			console.error(error.message);
@@ -67,7 +93,6 @@ require_once(__DIR__ . '/components/top.php');
 <?php
 require_once(__DIR__ . '/components/bottom.php');
 ?>
-<!-- TODO:style forgot password page like in amazon -->
-<!-- TODO:style new password page like in amazon -->
+
 <!-- TODO: if email sent successfully then show that (maybe redirect users or show options to redirect)-->
 <!-- TODO: if password changed successfully then show that and display options for redirecting-->
