d6cbf0cb86f6dc1c84a0fdffe14323bb8a1d15c1
klajdi44 <klajdi1996@yahoo.gr>
2021-12-01 23:54:49 +0100

    feature/added added edit email,name,phonenumber

---
+++ api/api_edit_account.php
@@ -0,0 +1,47 @@
+<?php
+require_once(__DIR__ . '/../private/globals.php');
+echo $_POST['user_name'];
+// **validation**
+//Name
+if (!isset($_POST['user_name'])) _res(400, ['info' => 'Name required', 'error' => __LINE__]);
+if (strlen($_POST['user_name']) < _USERNAME_MIN_LEN) _res(400, ['info' => 'Name must be at least ' . _USERNAME_MIN_LEN . ' characters long', 'error' => __LINE__]);
+if (strlen($_POST['user_name']) > _USERNAME_MAX_LEN) _res(400, ['info' => 'Name cannot be more than' . _USERNAME_MAX_LEN . ' characters long', 'error' => __LINE__]);
+
+// email
+if (!isset($_POST['user_email'])) _res(400, ['info' => 'email required', 'error' => __LINE__]);
+if (!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) _res(400, ['info' => 'Email is invalid', 'error' => __LINE__]);
+
+// phone number
+if (!isset($_POST['user_phone_number'])) _res(400, ['info' => 'Phone number required']);
+if (strlen($_POST['user_phone_number']) < _PHONE_LEN || strlen($_POST['user_phone_number']) > _PHONE_LEN) _res(400, ['info' => 'Phone number must be ' . _PHONE_LEN . ' characters long', 'error' => __LINE__]);
+if (!ctype_digit($_POST['user_phone_number'])) _res(400, ['info' => 'Phone number must contain only numbers', 'error' => __LINE__]);
+
+$db = _db();
+
+try {
+	// $query = $db->prepare('SELECT * FROM users WHERE user_id = :user_id');
+	// $query->bindValue(':user_id', $_SESSION['user_id']);
+	// $query->execute();
+	// $row = $query->fetch();
+	session_start();
+	$has_email_changed =  $_POST['user_email'] != $_SESSION['user_email'] ? true : false;
+
+	$query = $db->prepare('UPDATE users SET user_name = :user_name,user_email = :user_email, user_phone_number = :user_phone_number WHERE user_id = :user_id');
+	$query->bindValue(':user_id', $_SESSION['user_id']);
+	$query->bindValue(':user_name', $_POST['user_name']);
+	$query->bindValue(':user_email', $_POST['user_email']);
+	$query->bindValue(':user_phone_number', $_POST['user_phone_number']);
+	$query->execute();
+	$row = $query->rowCount();
+
+	if (!$row) {
+		_res(500, ['info' => 'Failed to update fields', 'error' => __LINE__]);
+	}
+
+	$_SESSION['user_name'] = $_POST['user_name'];
+	$_SESSION['user_email'] = $_POST['user_email'];
+	$_SESSION['user_phone_number'] = $_POST['user_phone_number'];
+	_res(200, ['info' => 'Success']);
+} catch (Exception $ex) {
+	_res(500, ['info' => 'system under maintainance', 'error' => __LINE__]);
+}
--- edit-account.php
+++ edit-account.php
@@ -11,29 +11,29 @@ if (!_is_user_signed_in()) {
 
 <main class='edit__account'>
 	<div class="edit__account__content__wrapper">
-		<h2 class="edit__account__title">Login & Security</h2>
+		<h2 class="edit__account__title">Name</h2>
+		<form id="form" onsubmit="return false" class="edit__account__container">
+			<legend class="edit__account__field user_name">
+				<label for="user_name">Full name</label>
+				<input oninput="enableSave()" id="user_name" class="user_name input" name="user_name" type="text" placeholder=" " value="<?= $_SESSION['user_name'] ?>">
+			</legend>
+			<legend class="edit__account__field user_email">
+				<label for="user_email">Email</label>
+				<input oninput="enableSave()" id="user_email" class="user_email input" name="user_email" type="text" placeholder=" " value="<?= $_SESSION['user_email'] ?>">
+			</legend>
+			<legend class="edit__account__field user_phone_number">
+				<label for="user_phone_number">Phone number</label>
+				<small>Only Danish numbers e.g 40636096</small>
+				<input oninput="enableSave()" id="user_phone_number" class="user_phone_number input" name="user_phone_number" type="tel" placeholder=" " value="<?= $_SESSION['user_phone_number'] ?>">
+			</legend>
+			<button onclick="editAccountInfo()" class="primary__btn edit__account__save__btn" disabled>Save</button>
+		</form>
+	</div>
+
+	<div class="edit__account__content__wrapper password__section">
+		<h2 class="edit__account__title">Password</h2>
+
 		<div class="edit__account__container">
-			<section class="edit__account__field user_name">
-				<article class="">
-					<strong>Name</strong>
-					<p><?= $_SESSION['user_name'] ?> </p>
-				</article>
-				<button class="secondary-button edit__account__edit__btn">Edit</button>
-			</section>
-			<section class="edit__account__field user_email">
-				<article>
-					<strong>Email</strong>
-					<p><?= $_SESSION['user_email'] ?> </p>
-				</article>
-				<button class="secondary-button edit__account__edit__btn">Edit</button>
-			</section>
-			<section class="edit__account__field user_phone_number">
-				<article class="">
-					<strong>Phone number</strong>
-					<p><?= $_SESSION['user_phone_number'] ?> </p>
-				</article>
-				<button class="secondary-button edit__account__edit__btn">Edit</button>
-			</section>
 			<section class="edit__account__field user_password">
 				<article class="">
 					<strong>Password</strong>
@@ -44,3 +44,38 @@ if (!_is_user_signed_in()) {
 		</div>
 	</div>
 </main>
+
+
+<script>
+	const user_name = document.querySelector('#user_name')
+	const user_phone_number = document.querySelector('#user_phone_number')
+	const user_email = document.querySelector('#user_email');
+
+	function enableSave() {
+		console.log(user_name.value);
+
+		const btn = document.querySelector('.edit__account__save__btn')
+		if (user_name.value.trim() !== '<?= $_SESSION['user_name'] ?>' || user_phone_number.value.trim() !== '<?= $_SESSION['user_phone_number'] ?>' || user_email.value.trim() !== '<?= $_SESSION['user_email'] ?>') {
+			return btn.removeAttribute('disabled');
+		}
+		btn.setAttribute('disabled', '');
+	}
+	async function editAccountInfo() {
+		const formData = new FormData();
+		formData.append('user_name', user_name.value.trim());
+		formData.append('user_email', user_email.value.trim());
+		formData.append('user_phone_number', user_phone_number.value.trim());
+
+		try {
+			const request = await fetch('./api/api_edit_account', {
+				method: 'POST',
+				body: formData
+			});
+			const response = await request.text();
+			console.log(response);
+		} catch (error) {
+			console.error(error.message);
+		}
+	}
+</script>
+<?php require_once(__DIR__ . '/components/bottom.php') ?>
