eed6b3051bf92e2feadf6c3158dd5ebcd4d97839
klajdi44 <klajdi1996@yahoo.gr>
2021-12-17 03:39:40 +0100

    added message for is_verified email sent

--- components/nav.php
+++ components/nav.php
@@ -20,26 +20,28 @@ if ($userSignedIn) {
 
 	<p class="nav__login">
 		<?php
-		echo $userSignedIn ?  "Hello, $username" : "Hello, sign in"
+		echo $userSignedIn ?  "Hello, $username" : ""
 		?>
 	</p>
+
 	<div class="nav__login__section__container">
-
-		<span class=<?= $userSignedIn ? 'nav__login__section hidden'  : 'nav__login__section' ?>>
-
-			<button class=" signup__button"><a href="./login"> Sign in </a></button>
-			<p class="signup"> New customer? <a href="./signup"> start here </a></p>
-		</span>
-
-
 		<div class="nav__dropdown">
 			<button class="nav__dropbtn primary__btn"><i class="fas fa-caret-down"></i></button>
 			<div class="nav__dropdown__content">
-				<?= $userSignedIn ? "<a href='./account' class='nav__link'>Account</a>" : '' ?>
-				<a href='./upload-product' class='nav__link'>Upload product</a>
-				<?= $userSignedIn ? "<a href='./bridges/logout' class='nav__signout nav__link'>Logout</a>" : '' ?>
+				<?php if ($userSignedIn) { ?>
+					<a href='./account' class='nav__link'>Account</a>
+					<a href='./upload-product' class='nav__link'>Upload product</a>
+					<a href='./bridges/logout' class='nav__signout nav__link'>Logout</a>
+				<?php } else { ?>
+					<a href="./login" class="nav__login__section nav__link"> Log in</a>
+					<a href="./signup" class="signup "> Sign up</p>
+
+					<?php } ?>
 			</div>
 		</div>
-	</div>
-
 </nav>
+</div>
+<?php if (!$_SESSION['is_verified'] && !$_SESSION['verified_msg_shown']) { ?>
+	<?php $_SESSION['verified_msg_shown'] = true ?>
+	<p class='nav__is__verified'> Account verification link has been sent to <?= $_SESSION['user_email'] ?>! </p>
+<?php } ?>
